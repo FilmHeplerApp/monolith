07f7cd138d4c462d024a3ff876dba24e5822529b
vladimir <bubnovv661@gmail.com>
2026-07-21 16:54:51 +0300

    clean_architecture. Created a clean application architecture + added a description to the docs folder

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,11 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withCommands([
+        __DIR__.'/../app/Interfaces/Console/Commands',
+    ])
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
